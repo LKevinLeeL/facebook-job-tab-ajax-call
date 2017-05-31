f23751ada1b35bb549bbb092962feac321abae6b
Kevin <LKevinLeeL@gmail.com>
2017-05-31 15:33:58 -0400

    changed app id and app key

--- ajax.js
+++ ajax.js
@@ -1,8 +1,8 @@
 $.ajax({
     url: "https://api.staging.jibe.com/v1/jobs",
     headers: {
-      "app_id": "6ff28ef5",
-      "app_key": "public-tyrell",
+      "app_id": "7077e7b9",
+      "app_key": "eaton",
       "Accept-Language": "en-us",
       "Accept": "application/json; charset=UTF-8"
     },
@@ -15,7 +15,7 @@ $.ajax({
       // console.log(JSON.stringify(data));
 
       for( i in data.jobs){
-			     $('#container').append('<div class="job-container"><div class="job-info"><p>'+ data.jobs[i].title +'</p><p>'+ data.jobs[i].city +', '+ data.jobs[i].state +'</p></div><a href="https://mortonfinancial.staging.jibeapply.com/jobs/'+ data.jobs[i].slug +'">Apply</a></div>');
+			     $('#container').append('<div class="job-container"><div class="job-info"><p>'+ data.jobs[i].title +'</p><p>'+ data.jobs[i].city +', '+ data.jobs[i].state +'</p></div><a href="https://eaton.staging.jibeapply.com/jobs/'+ data.jobs[i].slug +'" target="_blank">Apply</a></div>');
 		  }
     },
     error: function(data){
