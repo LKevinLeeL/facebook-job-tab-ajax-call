fdddd217b3353ac3bddd49a7eff989c0ca720309
Kevin <LKevinLeeL@gmail.com>
2017-05-31 14:36:09 -0400

    Git init

---
+++ ajax.js
@@ -0,0 +1,24 @@
+$.ajax({
+    url: "https://api.staging.jibe.com/v1/jobs",
+    headers: {
+      "app_id": "6ff28ef5",
+      "app_key": "public-tyrell",
+      "Accept-Language": "en-us",
+      "Accept": "application/json; charset=UTF-8"
+    },
+    method: "GET",
+    dataType: "json",
+    // data: {},
+    success: function(data) {
+      console.log('all', data);
+      console.log('firstTitle',data.jobs[0].city);
+      // console.log(JSON.stringify(data));
+
+      for( i in data.jobs){
+			     $('#jobs').append('<div><p>'+ data.jobs[i].title +'</p><p>'+ data.jobs[i].city +', '+ data.jobs[i].state +'</p><a href="https://mortonfinancial.staging.jibeapply.com/jobs/'+ data.jobs[i].slug +'">Apply</a></div>');
+		  }
+    },
+    error: function(data){
+		console.log('error',data);
+	}
+});
