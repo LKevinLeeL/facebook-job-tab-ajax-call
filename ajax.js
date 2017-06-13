bb7a78fd76d487e438f9e1d13b2858583fd57874
Kevin <LKevinLeeL@gmail.com>
2017-06-13 15:30:20 -0400

    More edits

--- ajax.js
+++ ajax.js
@@ -11,13 +11,57 @@ $.ajax({
     method: "GET",
     // data: {limit: numb_jobs },
     success: function(data) {
-      console.log('all', data);
+      // console.log('all', data);
+
+      var res = data.jobs_by_language["en-us"];
       // console.log('firstTitle',data.jobs[0].city);
       // console.log(JSON.stringify(data));
+      console.log('all', res);
 
-      for( i in data.jobs_by_language["en-us"]){
-			     $('#container').append('<div class="job-container"><div class="job-info"><p class="title"><a href="https://eaton.jibeapply.com/jobs/'+ data.jobs_by_language["en-us"][i].slug +'" target="_blank">'+ data.jobs_by_language["en-us"][i].title +'</a></p><p class="location">'+ data.jobs_by_language["en-us"][i].city +', '+ data.jobs_by_language["en-us"][i].state +', '+ data.jobs_by_language["en-us"][i].country +'</p></div><a class="applybtn" href="https://eaton.jibeapply.com/jobs/'+ data.jobs_by_language["en-us"][i].slug +'" target="_blank">Apply</a></div>');
+      var jobs = []
+
+      // for (var job in res) {
+      //   // console.log(res[job]);
+      //   var city = res[job].city;
+      //   var state = res[job].state;
+      //   var country = res[job].country;
+      //   var slug = res[job].slug;
+      //   var title = res[job].title;
+      //
+      //   var jobObject = {
+      //     slug: slug,
+      //     title: title
+      //   }
+      //
+      //   if (city) {
+      //     jobObject.city = city;
+      //   }
+      //   if (state) {
+      //     jobObject.state = state;
+      //   }
+      //   if (country) {
+      //     jobObject.country = country;
+      //   }
+      //   // console.log('OBJECT', jobObject);
+      //   jobs.push(jobObject)
+      // }
+      // console.log('testing',jobs);
+
+      function getLocation (location) {
+        if (location) {
+          return location + ", "
+        }
+        else {
+          return ""
+        }
+      }
+
+      for( i in res){
+			     $('#container').append('<div class="job-container"><div class="job-info"><p class="title"><a href="https://eaton.jibeapply.com/jobs/'+ res[i].slug +'" target="_blank">'+ res[i].title +'</a></p><p class="location">'+ getLocation(res[i].city) + getLocation(res[i].state) + res[i].country +'</p></div><a class="applybtn" href="https://eaton.jibeapply.com/jobs/'+ res[i].slug +'" target="_blank">Apply</a></div>');
 		  }
+
+
+
     },
     error: function(data){
 		console.log('error',data);
