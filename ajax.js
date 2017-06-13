fd400bf161fc92847960db7c3cc3ca8da4eac364
Kevin <LKevinLeeL@gmail.com>
2017-06-13 13:15:01 -0400

    limited 50 jobs

--- ajax.js
+++ ajax.js
@@ -1,21 +1,22 @@
 $.ajax({
-    url: "https://api.staging.jibe.com/v1/jobs",
+    url: "https://api.jibe.com/v1/jobs/find?limit=50",
+    // url: "https://api.jibe.com/v1/jobs",
     headers: {
       "app_id": "7077e7b9",
       "app_key": "eaton",
       "Accept-Language": "en-us",
-      "Accept": "application/json; charset=UTF-8"
+      "Accept": "application/json; charset=UTF-8",
     },
-    method: "GET",
     dataType: "json",
-    // data: {},
+    method: "GET",
+    // data: {limit: numb_jobs },
     success: function(data) {
       console.log('all', data);
-      console.log('firstTitle',data.jobs[0].city);
+      // console.log('firstTitle',data.jobs[0].city);
       // console.log(JSON.stringify(data));
 
-      for( i in data.jobs){
-			     $('#container').append('<div class="job-container"><div class="job-info"><p class="title"><a href="https://eaton.staging.jibeapply.com/jobs/'+ data.jobs[i].slug +'" target="_blank">'+ data.jobs[i].title +'</a></p><p class="location">'+ data.jobs[i].city +', '+ data.jobs[i].state +'</p></div><a class="applybtn" href="https://eaton.staging.jibeapply.com/jobs/'+ data.jobs[i].slug +'" target="_blank">Apply</a></div>');
+      for( i in data.jobs_by_language["en-us"]){
+			     $('#container').append('<div class="job-container"><div class="job-info"><p class="title"><a href="https://eaton.jibeapply.com/jobs/'+ data.jobs_by_language["en-us"][i].slug +'" target="_blank">'+ data.jobs_by_language["en-us"][i].title +'</a></p><p class="location">'+ data.jobs_by_language["en-us"][i].city +', '+ data.jobs_by_language["en-us"][i].state +', '+ data.jobs_by_language["en-us"][i].country +'</p></div><a class="applybtn" href="https://eaton.jibeapply.com/jobs/'+ data.jobs_by_language["en-us"][i].slug +'" target="_blank">Apply</a></div>');
 		  }
     },
     error: function(data){
