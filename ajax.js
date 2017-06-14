ccf38f7761225a5a1a742098326f6c9ff28b1bb3
Kevin <LKevinLeeL@gmail.com>
2017-06-14 13:16:17 -0400

    Add view more button

--- ajax.js
+++ ajax.js
@@ -20,33 +20,6 @@ $.ajax({
 
       var jobs = []
 
-      // for (var job in res) {
-      //   // console.log(res[job]);
-      //   var city = res[job].city;
-      //   var state = res[job].state;
-      //   var country = res[job].country;
-      //   var slug = res[job].slug;
-      //   var title = res[job].title;
-      //
-      //   var jobObject = {
-      //     slug: slug,
-      //     title: title
-      //   }
-      //
-      //   if (city) {
-      //     jobObject.city = city;
-      //   }
-      //   if (state) {
-      //     jobObject.state = state;
-      //   }
-      //   if (country) {
-      //     jobObject.country = country;
-      //   }
-      //   // console.log('OBJECT', jobObject);
-      //   jobs.push(jobObject)
-      // }
-      // console.log('testing',jobs);
-
       function getLocation (location) {
         if (location) {
           return location + ", "
@@ -59,8 +32,6 @@ $.ajax({
       for( i in res){
 			     $('#container').append('<div class="job-container"><div class="job-info"><p class="title"><a href="https://eaton.jibeapply.com/jobs/'+ res[i].slug +'" target="_blank">'+ res[i].title +'</a></p><p class="location">'+ getLocation(res[i].city) + getLocation(res[i].state) + res[i].country +'</p></div><a class="applybtn" href="https://eaton.jibeapply.com/jobs/'+ res[i].slug +'" target="_blank">Apply</a></div>');
 		  }
-
-
 
     },
     error: function(data){
